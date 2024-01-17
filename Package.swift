635af9fe5209dd239a61a312fd66ec554b145918
Marcin Gmyz <marcin.gmyz@apzumi.com>
2024-01-17 10:22:54 +0100

    Upgraded dependency version

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/FleksySDK/FleksySDK-iOS", exact: "4.17.1"),
+        .package(url: "https://github.com/FleksySDK/FleksySDK-iOS", exact: "4.19.1"),
         .package(url: "https://github.com/realm/realm-swift.git", exact: "10.42.1"),
     ],
     targets: [
